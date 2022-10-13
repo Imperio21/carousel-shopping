447b881eabbc0d18b184f17abeb37084d6c27880
Isabelle Império <isabelle.imperio@csptech.com.br>
2022-10-13 17:14:51 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
 
-function App() {
+import "./App.css";
+
+export function App() {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/static/shoes.json")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (!data || !data.length) return null;
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <img src="/static/images/super-shoes.png" alt="superShoes logo" />
+      </div>
+      <div className="carousel" ref={carousel}>
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image } = item;
+          return (
+            <div className="item" key={id}>
+              <div className="image">
+                <img src={image} alt={name} />
+              </div>
+              <div className="info">
+                <span className="name">{name} </span>
+                <span className="oldPrice">U${oldPrice} </span>
+                <span className="price">{price} </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}>
+          <img
+            src="/static/images/216151_right_chevron_icon.png"
+            alt="Scroll left"
+          />
+        </button>
+        <button onClick={handleRightClick}>
+          <img
+            src="/static/images/216151_right_chevron_icon.png"
+            alt="Scroll right"
+          />
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
